ce0d306134e172682bceef09c57de5ee7b5a0504
Hari <hrajaraman89@gmail.com>
2012-10-14 21:58:51 -0400

    eventadder and adder interface

---
+++ src/com/apps/smscal/services/CalendarEventAdder.java
@@ -0,0 +1,35 @@
+package com.apps.smscal.services;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.provider.CalendarContract.Events;
+
+import com.apps.smscal.model.CalendarInfo;
+import com.apps.smscal.model.EventInfo;
+
+public class CalendarEventAdder implements EventAdder {
+
+    private CalendarInfo calendarInfo;
+    private ContentResolver contentResolver;
+
+    public CalendarEventAdder(CalendarInfo info, ContentResolver resolver) {
+        this.calendarInfo = info;
+        this.contentResolver = resolver;
+    }
+
+    /* (non-Javadoc)
+     * @see com.apps.smscal.services.EventAdder#add(com.apps.smscal.model.EventInfo)
+     */
+    @Override
+    public void add(EventInfo info) {
+        ContentValues values = new ContentValues();
+        values.put(Events.DTSTART, info.getStartTime());
+        values.put(Events.DTEND, info.getEndTime());
+        values.put(Events.TITLE, info.getTitle());
+        values.put(Events.DESCRIPTION, info.getDescription());
+        values.put(Events.CALENDAR_ID, this.calendarInfo.getId());
+        values.put(Events.EVENT_TIMEZONE, info.getTimeZone());
+
+        this.contentResolver.insert(Events.CONTENT_URI, values);
+    }
+}
---
+++ src/com/apps/smscal/services/EventAdder.java
@@ -0,0 +1,9 @@
+package com.apps.smscal.services;
+
+import com.apps.smscal.model.EventInfo;
+
+public interface EventAdder {
+
+    public void add(EventInfo info);
+
+}
